ebf976a2d5589b48d34ffc67c4e40b63eb31c9bf
KolekarPramod <pkolekar940@gmail.com>
2025-06-25 16:49:08 +0530

    added multiply function for new branch fix typo

--- app2.py
+++ app2.py
@@ -6,7 +6,12 @@ def subtract(a, b):
     return a - b
 
 
+def multiply(a, b):
+    return a * b
+
+
 print("Welcome to the Calculator App")
 
 print(add(5, 3))  # Example usage of the add function
 print(subtract(10, 4))  # Example usage of the subtract function
+print(multiply(6, 7))  # Example usage of the multiply function
