2f2b28d776632123abb028d1befe5cddb6e1fe0b
KolekarPramod <pkolekar940@gmail.com>
2025-06-25 19:02:58 +0530

    added test cases

--- app2.py
+++ app2.py
@@ -2,11 +2,11 @@ def add(a, b):
     return a + b
 
 
-def subtract(a, b):
+def sub(a, b):
     return a - b
 
+if __name__ == '__main__':
+    print("Welcome to the Calculator App")
 
-print("Welcome to the Calculator App")
-
-print(add(5, 3))  # Example usage of the add function
-print(subtract(10, 4))  # Example usage of the subtract function
+    print(add(5, 3))  # Example usage of the add function
+    print(sub(10, 4))  # Example usage of the subtract function
---
+++ test_app.py
@@ -0,0 +1,17 @@
+# test_app.py
+
+import unittest
+from app2 import add, sub
+
+class TestMathFunctions(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(4, 5), 9)
+        self.assertEqual(add(-1, 1), 0)
+
+    def test_sub(self):
+        self.assertEqual(sub(4, 5), -1)
+        self.assertEqual(sub(-1, -1), 0)
+
+if __name__ == '__main__':
+    unittest.main()
