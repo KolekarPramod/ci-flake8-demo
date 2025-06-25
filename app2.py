a6dd89f90d077878a0345ffb08513e84fd88d02d
KolekarPramod <pkolekar940@gmail.com>
2025-06-25 16:12:07 +0530

    added new correct file

---
+++ app2.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+print("Welcome to the Calculator App")
+
+print(add(5, 3))  # Example usage of the add function
+print(subtract(10, 4))  # Example usage of the subtract function
